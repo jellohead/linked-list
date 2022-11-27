eb4d1995be5832d4e0a4ea319649301b5c7b6513
Pat Escalona <pat.escalona7@gmail.com>
2022-11-27 15:52:51 -0600

    Basic structure in place

--- linked-list.js
+++ linked-list.js
@@ -1,1 +1,43 @@
 // linked-list.js
+
+class LinkedList {
+    constructor(value) {
+        this.value = value;
+    }
+
+    append() {
+        console.log('in LinkedList.append')
+    }
+
+    prepend() { }
+
+    size() { }
+
+    head() { }
+
+    tail() { }
+
+    at(index) { }
+
+    pop() { }
+
+    contains(value) { }
+
+    find(value) { }
+
+    toString() { }
+
+}
+
+class Node {
+    constructor(element) {
+        this.element = null;
+        this.next = null;
+    }
+}
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    console.log('DOM loaded and parsed');
+})
+
+let list = new LinkedList('things');
